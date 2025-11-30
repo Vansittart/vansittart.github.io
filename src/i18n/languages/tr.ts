88e7c7b3d822c6a344112eb81b38d2fd6d085da1
Svecco (Chenrui Wu) <chenrui@svecco.org>
2025-11-30 12:00:00 +0800

    init(framework): blog of svecco (chenrui wu)

---
+++ src/i18n/languages/tr.ts
@@ -0,0 +1,40 @@
+import Key from "../i18nKey";
+import type { Translation } from "../translation";
+
+export default {
+	[Key.home]: "Ana Sayfa",
+	[Key.about]: "Hakkında",
+	[Key.archive]: "Arşiv",
+	[Key.progress]: "İlerleme",
+	[Key.search]: "Ara",
+	[Key.loading]: "Yükleniyor",
+
+	[Key.tags]: "Etiketler",
+	[Key.categories]: "Kategoriler",
+	[Key.recentPosts]: "Son Gönderiler",
+
+	[Key.comments]: "Yorumlar",
+
+	[Key.untitled]: "Başlıksız",
+	[Key.uncategorized]: "Kategorize Edilmemiş",
+	[Key.noTags]: "Etiket Yok",
+
+	[Key.wordCount]: "kelime",
+	[Key.wordsCount]: "kelime",
+	[Key.minuteCount]: "dakika",
+	[Key.minutesCount]: "dakika",
+	[Key.postCount]: "gönderi",
+	[Key.postsCount]: "gönderi",
+
+	[Key.themeColor]: "Tema Rengi",
+
+	[Key.lightMode]: "Açık",
+	[Key.darkMode]: "Koyu",
+	[Key.systemMode]: "Sistem",
+
+	[Key.more]: "Daha fazla",
+
+	[Key.author]: "Yazar",
+	[Key.publishedAt]: "Yayınlandı",
+	[Key.license]: "Lisans",
+} as Translation;
---
+++ src/i18n/languages/zh_TW.ts
@@ -0,0 +1,40 @@
+import Key from "../i18nKey";
+import type { Translation } from "../translation";
+
+export default {
+	[Key.home]: "首頁",
+	[Key.about]: "關於",
+	[Key.archive]: "彙整",
+	[Key.progress]: "進展",
+	[Key.search]: "搜尋",
+	[Key.loading]: "載入中",
+
+	[Key.tags]: "標籤",
+	[Key.categories]: "分類",
+	[Key.recentPosts]: "最新文章",
+
+	[Key.comments]: "評論",
+
+	[Key.untitled]: "無標題",
+	[Key.uncategorized]: "未分類",
+	[Key.noTags]: "無標籤",
+
+	[Key.wordCount]: "字",
+	[Key.wordsCount]: "字",
+	[Key.minuteCount]: "分鐘",
+	[Key.minutesCount]: "分鐘",
+	[Key.postCount]: "篇文章",
+	[Key.postsCount]: "篇文章",
+
+	[Key.themeColor]: "主題色",
+
+	[Key.lightMode]: "淺色",
+	[Key.darkMode]: "深色",
+	[Key.systemMode]: "跟隨系統",
+
+	[Key.more]: "更多",
+
+	[Key.author]: "作者",
+	[Key.publishedAt]: "發佈於",
+	[Key.license]: "許可證",
+} as Translation;
